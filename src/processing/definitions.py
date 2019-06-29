ecb067c96c6aa9e53b4ccd5a526bc22dc7dc0824
spencerchan <spencerchan@uchicago.edu>
2019-06-29 14:49:42 -0500

    added cta time periods and bunching directory defs

--- src/processing/definitions.py
+++ src/processing/definitions.py
@@ -16,10 +16,16 @@ VEHICLES_DIR = os.path.join(RAW_DATA_DIR, "getvehicles")
 ## Processed data
 TIMETABLES_DIR = os.path.join(PROCESSED_DATA_DIR, "timetables")
 TRAVELS_WAITS_DIR = os.path.join(PROCESSED_DATA_DIR, "travels_waits")
+BUNCHING_DIR = os.path.join(PROCESSED_DATA_DIR, "bunching")
+
+## Date/Time Definitions 
+CTA_TIME_PERIODS = [6, 9, 15, 19, 22]
+CTA_TIME_PERIOD_LABELS = ["AM Peak", "Midday", "PM Peak", "Evening"]
+
 # Holiday schedules
 # Our services operate on a Sunday schedule on New Year's Day, Memorial Day,
 # July 4th (Independence Day), Labor Day, Thanksgiving Day and Christmas Day.
-HOLIDAYS = [
+CTA_HOLIDAYS = [
   "2017-01-01", "2017-05-29", "2017-07-04", "2018-09-04", "2018-11-23", "2017-12-25",
   "2018-01-01", "2018-05-28", "2018-07-04", "2018-09-03", "2018-11-22", "2018-12-25",
   "2019-01-01", "2019-05-27", "2019-07-04", "2019-09-02", "2019-11-28", "2019-12-25"
