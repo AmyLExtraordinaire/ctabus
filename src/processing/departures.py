7f47552e8ddae2cb3995f0c8055a1cb5ca5a14ad
spencerchan <spencerchan@uchicago.edu>
2019-07-12 21:48:34 -0500

    outputs hourly departures as percentage obs/days

--- src/processing/departures.py
+++ src/processing/departures.py
@@ -28,34 +28,40 @@ def process(df, origin):
   df["bin"] = pd.cut(df["min"], bins=bins, right=False, labels=labels)
   return df
 
-def build_hourly_departures(df, first_last):
+def build_hourly_departures(df, terminals):
   json_obj = {}
   for pid in df.pid.unique():
     by_pid = []
-    origin_stpid = first_last.loc[first_last.pid == pid, "stpid"].unique()[0]
+    origin_stpid = terminals.loc[terminals.pid == pid, "stpid"].unique()[0]
     tmp = df.loc[df.pid == pid].copy()
     tmp = process(tmp, origin_stpid)
     for d in ["W", "S", "U"]:
+      if d not in tmp.dow.unique():
+        continue
+      num_days = tmp.groupby(["dow"]).nunique().start_date.reset_index()
       hourly_departures = []
+      hr_avgs = tmp[(tmp.dow == d)].groupby(["dow", "pid", "hr", "start_date"]).count().unstack().fillna(0).stack().groupby(["dow", "pid", "hr"])[origin_stpid].mean().reset_index().round(1)
       for h in sorted(tmp[~pd.isnull(tmp[origin_stpid]) & (tmp.dow == d)].hr.unique()):
-        hourly_avg = tmp[(tmp.dow == d) & (tmp.hr == h)].groupby(["dow", "pid", "hr", "start_date"]).count().groupby(["dow", "pid", "hr"])[origin_stpid].mean().reset_index().round(1)
-        hourly_departures.append({"hr": int(h), "departures": tmp[(tmp.dow == d) & (tmp.hr == h)]["bin"].value_counts().sort_index().tolist(), "avg": float(hourly_avg.loc[0, origin_stpid])})
+        departures = (tmp[(tmp.dow == d) & (tmp.hr == h)]["bin"].value_counts().sort_index() / float(num_days.loc[num_days.dow == d, "start_date"])).tolist()
+        avg = float(hr_avgs.loc[hr_avgs.hr == h, origin_stpid])
+        hourly_departures.append({"hr": int(h), "departures": departures, "avg": avg})
       by_pid.append({"stpid": origin_stpid, "dow": d, "data": hourly_departures})
     json_obj[str(pid)] = by_pid
   return json_obj
 
-def build_daily_departures(df, first_last):
+
+
+def build_daily_departures(df, terminals):
   daily_depatures = []
-  # sort rtdir alphabetically?
-  for rtdir in first_last.rtdir.unique():
+  for rtdir in sorted(terminals.rtdir.unique()):
     data = []
-    for pid in first_last[first_last.rtdir == rtdir].pid.unique():
-      origin = first_last.loc[first_last.pid == pid, "stpnm_x"].unique()[0]
-      origin_stpid = first_last.loc[first_last.pid == pid, "stpid"].unique()[0]
-      destination = first_last.loc[first_last.pid == pid, "stpnm_y"].unique()[0]
-      daily = df[df.pid == pid].groupby(["dow", "start_date"]).count().groupby(["dow"])[origin_stpid].mean().reset_index().rename(columns={origin_stpid: "counts"})
-      daily.counts = daily.counts.round().astype(int)
-      data.append({"pid": int(pid), "origin": origin, "destination": destination, "counts": list(daily.counts)})
+    for pid in terminals[terminals.rtdir == rtdir].pid.unique():
+      origin_stpid = terminals.loc[terminals.pid == pid, "stpid"].unique()[0]
+      daily = df[df.pid == pid].groupby(["dow", "pid", "start_date"])[origin_stpid].count().groupby(["dow", "pid"]).mean().reset_index().round().rename(columns={origin_stpid: "counts"})
+      origin = terminals.loc[terminals.pid == pid, "stpnm_x"].unique()[0]
+      destination = terminals.loc[terminals.pid == pid, "stpnm_y"].unique()[0]
+      counts = {d.lower(): int(daily.loc[daily.dow == d, "counts"]) if d in list(daily.dow) else 0 for d in ["W", "S", "U"]}
+      data.append({"pid": int(pid), "origin": origin, "destination": destination, "counts": counts})
     daily_depatures.append({"direction": rtdir, "data": data})
   return daily_depatures
 
@@ -66,7 +72,6 @@ def write_departures(json_obj, rt, tag):
     json.dump(json_obj, f)
 
 def main(rt):
-  # do this with timetables instead of tw?
   timetables_path = os.path.join(definitions.TIMETABLES_DIR, "{}_*_timetable.csv".format(rt))
   tt = tools.load_all_dfs(timetables_path)
   tt = preprocess(tt)
@@ -77,14 +82,14 @@ def main(rt):
   first_stops = patterns.loc[idxmins]
   idxmaxs = g.seq.idxmax()
   last_stops = patterns.loc[idxmaxs]
-  first_last = first_stops.merge(last_stops[["pid", "stpnm"]], on='pid')
+  terminals = first_stops.merge(last_stops[["pid", "stpnm"]], on='pid')
 
   tt[list(first_stops.stpid)] = tt[list(first_stops.stpid)].apply(pd.to_datetime)
 
-  hourly_departures = build_hourly_departures(tt, first_last)
-  #daily_departures = build_daily_departures(tt, first_last)
+  hourly_departures = build_hourly_departures(tt, terminals)
+  daily_departures = build_daily_departures(tt, terminals)
   write_departures(hourly_departures, rt, "hourly")
-  #write_departures(daily_departures, rt, "daily")
+  write_departures(daily_departures, rt, "daily")
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
